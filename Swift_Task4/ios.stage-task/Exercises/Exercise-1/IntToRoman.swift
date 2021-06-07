0de0ac9d7416a6d661945c2398f43ae069af05b6
Evgeniy Gidzinskiy <50-G-Unit@mail.ru>
2021-06-08 03:10:28 +0700

    feat: solved 1st and 2nd exercises, 3rd not solved (i haven't time for this exercise)

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,24 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        
+        let romanNumbers = [1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X",
+                            40: "XL", 50: "L", 90: "XC", 100: "C",
+                            400: "CD", 500: "D", 900: "CM", 1000: "M"]
+        
+        if self <= 0 || self > 3999 {
+            return nil
+        } else {
+            var result : String = ""
+            var intNum = self
+
+            for key in romanNumbers.keys.sorted(by: >) {
+                while intNum >= key {
+                    intNum -= key
+                    result.append(romanNumbers[key]!)
+                }
+            }
+        return result
+        }
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -3,6 +3,32 @@ import Foundation
 final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        
+        guard isValid(row: row, column: column, for: image) else { return image }
+        guard image.count > 0, image.first!.count > 0 else { return image }
+                
+        let oldColor = image[row][column]
+        guard oldColor != newColor else { return image }
+        
+        var newImage = image
+        newImage[row][column] = newColor
+                
+        let neighbours = [(row + 1, column),
+                        (row - 1, column),
+                        (row, column - 1),
+                        (row, column + 1)]
+                
+        for (nRow, nColumn) in neighbours {
+            guard isValid(row: nRow, column: nColumn, for: image) else { continue }
+        
+            if image[nRow][nColumn] == oldColor {
+                newImage = fillWithColor(newImage, nRow, nColumn, newColor)
+            }
+        }
+            return newImage
+    }
+    
+    func isValid(row: Int, column: Int, for image: [[Int]]) -> Bool {
+        return row >= 0 && column >= 0 && row < image.count && column < image.first!.count
     }
 }
